6b019be7b3522cc7d0c9827543639c78afeeb176
sulikcovert404-beep <sulikcovert404-beep@users.noreply.github.com>
2026-09-21 02:13:59 +0330

    chore: remove safe unused imports

--- app/repositories/school_class.py
+++ app/repositories/school_class.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.models import ClassMembership, Classroom, SchoolTenant, StudentProfile, TeacherProfile
+from app.db.models import ClassMembership, Classroom, SchoolTenant
 
 
 class SchoolRepository:
